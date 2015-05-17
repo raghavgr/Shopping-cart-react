ea817edf575eaa8b069a37dcfd513435ccb114db
raghavgr94 <raghavgr94@gmail.com>
2015-05-17 02:18:34 -0700

    :zap: added state to counter component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,20 +1,27 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0
+  };
   render() {
     return (
       <React.Fragment>
-        <h1>Hello World</h1>
+        <span>{this.formatCount()}</span>
         <button>Increment</button>
       </React.Fragment>
     );
-    // Automatic semicolon insertion.
-    // In js if you have return and nothing next to it, a semicolon is added automatically.
-    // That's why create long JSX expressions with parentheses
+  }
 
-    // React.Fragment can be used to specify a component to be the child of another element
-    // Especially if thr parent is also another div, it is not needed to use another div
-    // within the component
+  formatCount() {
+    const { count } = this.state;
+    // picking the count property of this object and
+    // storing it in separate property called count
+    return count === 0 ? <h1>Zero</h1> : count;
+
+    // can return JSx expression, the html code above
+    // JSx are normal javaScript objects, they can be passed to a function
+    // returned from a function, and also define them as const variable
   }
 }
 
