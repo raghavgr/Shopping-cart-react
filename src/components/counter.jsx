ae3d41dddf385d5bd61bf21620885393c3f7072f
raghavgr94 <raghavgr94@gmail.com>
2019-05-16 00:08:32 -0700

    :art: change DOM structure by removing extra div through introduction of React.Fragment

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,19 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   render() {
-    return <h1>Hello World</h1>;
+    return (
+      <React.Fragment>
+        <h1>Hello World</h1>
+        <button>Increment</button>
+      </React.Fragment>
+    );
+    // Automatic semicolon insertion.
+    // In js if you have return and nothing next to it, a semicolon is added automatically.
+    // That's why create long JSX expressions with parentheses
+
+    // React.Fragment can be used to specify a component to be the child of another element
+    // Especially if thr parent is also another div, it is not needed to use another div
+    // within the component
   }
 }
 
