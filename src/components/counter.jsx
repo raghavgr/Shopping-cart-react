201af43533b34dee5c998815d326e91756b8243c
raghavgr94 <raghavgr94@gmail.com>
2019-05-17 04:17:13 -0700

    :art: styling added to jsx expressions

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,22 +2,34 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 0,
+    imageUrl: "https://picsum.photos/200"
   };
+
+  styles = {
+    fontSize: 15,
+    fontWeight: "bold"
+  };
+  // define styles property here
+  // and reference them in the requried JSx expression
   render() {
     return (
       <React.Fragment>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span style={this.styles} className="badge badge-primary m-2">
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
   }
+  // can't use a class property in object becuase it is a reserved keyword in JS
+  // hence we use className
 
   formatCount() {
     const { count } = this.state;
     // picking the count property of this object and
     // storing it in separate property called count
-    return count === 0 ? <h1>Zero</h1> : count;
+    return count === 0 ? "Zero" : count;
 
     // can return JSx expression, the html code above
     // JSx are normal javaScript objects, they can be passed to a function
