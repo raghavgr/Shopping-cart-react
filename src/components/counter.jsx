cb5c6cc661fa1b18cbc16e5e261d0acc9274de0e
raghavgr94 <raghavgr94@gmail.com>
2019-05-17 04:27:13 -0700

    :art: added dynamic classname change capability using getBadgeClasses

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,28 +2,23 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    imageUrl: "https://picsum.photos/200"
+    count: 0
   };
 
-  styles = {
-    fontSize: 15,
-    fontWeight: "bold"
-  };
-  // define styles property here
-  // and reference them in the requried JSx expression
   render() {
     return (
       <React.Fragment>
-        <span style={this.styles} className="badge badge-primary m-2">
-          {this.formatCount()}
-        </span>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
   }
-  // can't use a class property in object becuase it is a reserved keyword in JS
-  // hence we use className
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
 
   formatCount() {
     const { count } = this.state;
