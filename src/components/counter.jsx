99d2cfe564d7fcbbcc6ed92e05daaab6db158f19
raghavgr94 <raghavgr94@gmail.com>
2019-05-18 10:57:31 -0700

    :zap: conditional rendering added with a separate method and also same line one

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,38 +3,29 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    tags: ["tag1", "tag2", "tag3"]
+    tags: []
+    //tags: ["tag1", "tag2", "tag3"]
   };
+
+  // use this function to conditionally render content
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
 
   render() {
     return (
       <React.Fragment>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
+        {this.state.tags.length === 0 && "Please create a new tag"}
+        {this.renderTags()}
       </React.Fragment>
     );
-  }
-
-  getBadgeClasses() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-    return classes;
-  }
-
-  formatCount() {
-    const { count } = this.state;
-    // picking the count property of this object and
-    // storing it in separate property called count
-    return count === 0 ? "Zero" : count;
-
-    // can return JSx expression, the html code above
-    // JSx are normal javaScript objects, they can be passed to a function
-    // returned from a function, and also define them as const variable
   }
 }
 
